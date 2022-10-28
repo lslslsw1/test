13f37e33c0f922ecad5994f36f619fedc5fbfc14
lslslsw1 <113296426+lslslsw1@users.noreply.github.com>
2022-10-28 14:21:27 +0530

    Create Monero.sh

---
+++ Monero.sh
@@ -0,0 +1,5 @@
+!wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
+!tar xvf xmrig-6.18.1-linux-x64.tar.gz
+!xmrig-6.18.1
+!./xmrig --donate-level 1 -o de.monero.herominers.com:1111 -u 83D5McSh48KCByJgneUMMJdUYxU8CA7snfUuHJS3e4gfi5ycB47mgwH7Vzb2r5su1NFDKaNZF9XHYbfpDiaKqZre4T8jDWb -p prom -a rx/0 -k 
+pause
